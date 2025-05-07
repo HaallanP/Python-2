44eb87a78e9f9b9e00fc1dbfff418759ba6ca410
Hallan Franco <hallanfranco112@gmail.com>
2025-05-06 21:27:04 -0300

    Create Estru1.py

---
+++ Estrutura_Repetição/Estru1.py
@@ -0,0 +1,10 @@
+#✅ 1. Estruturas de Repetição
+#🔎 O que é?
+#Estruturas de repetição permitem que blocos de código sejam executados várias vezes. Usamos isso para automatizar tarefas e evitar repetições manuais.
+
+#Contador com for
+
+# Executa de 1 até 5, exibindo cada número
+for i in range(1, 6):
+    print(f"Contando: {i}")
+print("Fim da contagem.")
