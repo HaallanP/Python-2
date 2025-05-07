2e8a7c8c97e6c48d077314047bdeff24e83220c0
Hallan Franco <hallanfranco112@gmail.com>
2025-05-06 21:31:58 -0300

    Create Funções.py

---
+++ Funções/Funções.py
@@ -0,0 +1,9 @@
+# O que é?
+# Funções são blocos reutilizáveis de código. Elas ajudam a organizar, simplificar e reaproveitar a lógica de um programa.
+
+#Função Simples 
+
+def saudacao():
+    print("Olá, bem-vindo ao programa!")
+
+saudacao()
